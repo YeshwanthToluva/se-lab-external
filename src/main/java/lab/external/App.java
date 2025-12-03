a84561873982e6dfb0f07bc3b5781930d0d1fa98
Yeshwanth Toluva <96023129+YeshwanthToluva@users.noreply.github.com>
2025-12-03 13:59:04 +0530

    final changes

---
+++ src/main/java/lab/external/App.java
@@ -0,0 +1,8 @@
+package com.lab.external;
+
+   public class App {
+       public static void main(String[] args) {
+           // Print a message to the console
+           System.out.println("Hello, World! This is the lab-external Maven project.");
+       }
+   }
